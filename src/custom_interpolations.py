30a3ec1ff4e422ce6a4813944433fe8c3cb44b17
Charles <charles.wilmot2@hotmail.fr>
2020-09-14 11:24:47 +0200

    fix 5

--- src/custom_interpolations.py
+++ src/custom_interpolations.py
@@ -4,3 +4,4 @@ from omegaconf import OmegaConf
 OmegaConf.register_resolver("plus", lambda a,b: int(a) + int(b))
 OmegaConf.register_resolver("times", lambda a,b: int(a) * int(b))
 OmegaConf.register_resolver("plus_times", lambda a,b,c: (int(a) + int(b)) * int(c))
+OmegaConf.register_resolver("slash_to_dot", lambda s: s.replace('/', '.'))
